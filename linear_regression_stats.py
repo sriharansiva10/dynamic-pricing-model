3cbec17c23e4bf8f5444d449b75ab573577d5489
sriharansiva10 <sriharansiva242@gmail.com>
2026-06-17 12:59:37 +0530

    plotiing errors are fixed

--- linear_regression_stats.py
+++ linear_regression_stats.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+import numpy as np
 
 #readind the data from the csv file
-csv=pd.read_csv('ebike_dynamic_pricing_dataset_without_weather.csv')
+head_csv=pd.read_csv('ebike_dynamic_pricing_dataset_without_weather.csv').head(1000)
 
 # plot for first ten rows
-head_csv=csv.head(5)
 
 # linear regression plotting
 slope,intercept,r,p,stderr=stats.linregress(head_csv['fuel_price'],head_csv['rental_price']) #plotting between fuel price and rental price
@@ -21,10 +21,10 @@ linear_regression=list(map(func,head_csv['fuel_price']))
 
 #plotting
 #stop the plotting if the correlation is less than 0.6 or greater than -0.6
-if abs(r) > 0.6 :
+if abs(r) > 0.4 :
+    plt.subplot(1,2,1)
     plt.scatter(head_csv['fuel_price'],head_csv['rental_price'],color='#1010e3')
     plt.plot(head_csv['fuel_price'],linear_regression,color='#13e80c')
-    plt.subplot(1,2,1)
 else :
     print("no correlation between fuel price and rental price")
 
@@ -32,8 +32,8 @@ slope,intercept,r,p,stderr=stats.linregress(head_csv['fuel_price'],head_csv['dem
 
 linear_regression=list(map(func,head_csv['fuel_price']))
 
-if abs(r) > 0.6 :
+if abs(r) > 0.4 :
+    plt.subplot(1,2,2)
     plt.scatter(head_csv['fuel_price'],head_csv['demand_score'])
     plt.plot(head_csv['fuel_price'],linear_regression,color='#13e80c')
-    plt.subplot(1,2,2)
     plt.show()
